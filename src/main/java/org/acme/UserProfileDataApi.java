d4252846f75dad12cce17e9c7ce14e715d0de151
Helber Belmiro <helber.belmiro@gmail.com>
2023-01-04 12:25:01 -0300

    Added reactive code
    Signed-off-by: Helber Belmiro <helber.belmiro@gmail.com>

---
+++ src/main/java/org/acme/UserProfileDataApi.java
@@ -0,0 +1,52 @@
+package org.acme;
+
+import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.ws.rs.BeanParam;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.FormParam;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.MediaType;
+import java.io.File;
+import java.util.UUID;
+
+@Path("")
+@RegisterRestClient(baseUri="http://my.endpoint.com/api/v1", configKey="multipart_requests_yml")
+@ApplicationScoped
+public interface UserProfileDataApi {
+
+    @POST
+    @Path("/base64")
+    @Consumes({"multipart/form-data"})
+    void postBase64Data(
+            @BeanParam PostBase64DataMultipartForm multipartForm
+    );
+
+    class PostBase64DataMultipartForm {
+        @FormParam("encodedFile")
+        @org.jboss.resteasy.reactive.PartFilename("encodedFileFile")
+        @org.jboss.resteasy.reactive.PartType(MediaType.APPLICATION_OCTET_STREAM)
+        public String encodedFile;
+    }
+    @POST
+    @Path("/user-profile-data")
+    @Consumes({"multipart/form-data"})
+    void postUserProfileData(
+            @BeanParam PostUserProfileDataMultipartForm multipartForm
+    );
+
+    class PostUserProfileDataMultipartForm {
+        @FormParam("id")
+        @org.jboss.resteasy.reactive.PartType(MediaType.TEXT_PLAIN)
+        public UUID id;
+        @FormParam("address")
+        @org.jboss.resteasy.reactive.PartType(MediaType.APPLICATION_JSON)
+        public Address address;
+        @FormParam("profileImage")
+        @org.jboss.resteasy.reactive.PartFilename("profileImageFile")
+        @org.jboss.resteasy.reactive.PartType(MediaType.APPLICATION_OCTET_STREAM)
+        public File profileImage;
+    }
+}
